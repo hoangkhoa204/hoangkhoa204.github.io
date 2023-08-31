6cc973f8bb9993d65b1ca56dfcc1939d30d50c81
caylio2004 <hoangkhoa204@yahoo.com>
2023-08-30 20:15:16 -0700

    fixed histogram

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,9 +51,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "076d9b3b5ec137403ef0c541c7f6f95c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "75251d1648b27fee3ee840c968960ba1",
-"/": "75251d1648b27fee3ee840c968960ba1",
-"main.dart.js": "a75e5688959eea3b292359f61c5c4734",
+"index.html": "1426381e98a50fda0ad5cc79a1f7ed06",
+"/": "1426381e98a50fda0ad5cc79a1f7ed06",
+"main.dart.js": "beca45f25f477d78dc786a6471ea5a79",
 "manifest.json": "09e408e9ba28d8afa9b7f36898249ad7",
 "version.json": "a47e180c6ea035114d1d8f8cca00d414"};
 // The application shell files that are downloaded before a service worker can
